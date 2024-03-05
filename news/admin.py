b3e428396ca2c941b1333e1255760b5a09a711d4
devilsoumya2 <161450964+devilsoumya2@users.noreply.github.com>
2024-03-05 05:51:43 -0800

    Add files via upload

---
+++ news/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from news.models import News
+
+
+class Newsadmin(admin.ModelAdmin):
+    list_display  =('news_title','news_desc','news_slug','news_image')
+
+admin.site.register(News,Newsadmin)
+
+# Register your models here.
